7b78991ed3d4d05148a11d21d7bca66951af01bf
celotts <clott@adexus.cl>
2019-04-28 18:57:15 -0430

    Mjoras form

--- form-angular-reactive/src/app/routes/form/form-detail/form-detail.component.ts
+++ form-angular-reactive/src/app/routes/form/form-detail/form-detail.component.ts
@@ -1,16 +1,29 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl } from '@angular/forms';
-
+import { FormControl, FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { states } from 'src/app/data-model';
 @Component({
   selector: 'app-form-detail',
   templateUrl: './form-detail.component.html',
   styleUrls: ['./form-detail.component.scss']
 })
 export class FormDetailComponent implements OnInit {
-  name = new FormControl();
-  constructor() { }
+  heroForm: FormGroup;
+  states = states;
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit() {
+    this.createrForm();
+  }
+  createrForm() {
+    this.heroForm = this.fb.group({
+      name: ['', Validators.required ],
+      street: '',
+      city: '',
+      state: '',
+      zip: '',
+      power: 'x-ray vision',
+      sidekick: true
+    });
   }
 
 }
